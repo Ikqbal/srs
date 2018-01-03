7a038c091a55d3a2147bfdf11ff7f4386474a6e9
Ikqbal <ikqbal141297@gmail.com>
2018-01-03 08:54:54 +0800

    Done

--- student/models.py
+++ student/models.py
@@ -6,16 +6,16 @@ class Student(models.Model):
 
 	PROGRAMMING = 'PR'
 	NETWORKING = 'NT'
-	PCTECT = 'PC'
-	COUSE_CHOICES = (
+	PCTECH = 'PC'
+	COURSE_CHOICES = (
 		(PROGRAMMING, 'Programming'),
 		(NETWORKING, 'Networking'),
-		(PCTECT, 'PC Technichian'),
+		(PCTECH, 'PC Technichian'),
 	)
 
 	icnum = models.CharField('IC Number',max_length=12,unique=True,blank=False,null=False)
 	name = models.CharField('Name',max_length=300,blank=False,null=False)
-	course = models.CharField('Coursework',max_length=2,choices=COUSE_CHOICES,default=PCTECT)
+	course = models.CharField('Coursework',max_length=2,choices=COURSE_CHOICES,default=PCTECH)
 	createdby = models.ForeignKey('auth.User',on_delete=models.CASCADE)
 	created_date = models.DateTimeField('Created Date',auto_now_add=True)
 
